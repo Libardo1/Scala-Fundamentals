4fd71e0b352d914397c1df4c87b215a0112cf334
Marvin Bertin <marvin.bertin@gmail.com>
2016-01-17 14:11:40 -0800

    tweetSet

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -35,84 +35,30 @@ class Tweet(val user: String, val text: String, val retweets: Int) {
  */
 abstract class TweetSet {
 
-  /**
-   * This method takes a predicate and returns a subset of all the elements
-   * in the original set for which the predicate is true.
-   *
-   * Question: Can we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
-   */
-    def filter(p: Tweet => Boolean): TweetSet = ???
-  
-  /**
-   * This is a helper method for `filter` that propagetes the accumulated tweets.
-   */
+  def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
+
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
 
-  /**
-   * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
-   *
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
-   */
-    def union(that: TweetSet): TweetSet = ???
-  
-  /**
-   * Returns the tweet from this set which has the greatest retweet count.
-   *
-   * Calling `mostRetweeted` on an empty set should throw an exception of
-   * type `java.util.NoSuchElementException`.
-   *
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
-   */
-    def mostRetweeted: Tweet = ???
-  
-  /**
-   * Returns a list containing all tweets of this set, sorted by retweet count
-   * in descending order. In other words, the head of the resulting list should
-   * have the highest retweet count.
-   *
-   * Hint: the method `remove` on TweetSet will be very useful.
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
-   */
-    def descendingByRetweet: TweetList = ???
-  
-  /**
-   * The following methods are already implemented
-   */
+  def union(that: TweetSet): TweetSet
 
-  /**
-   * Returns a new `TweetSet` which contains all elements of this set, and the
-   * the new element `tweet` in case it does not already exist in this set.
-   *
-   * If `this.contains(tweet)`, the current set is returned.
-   */
+  def mostRetweeted: Tweet
+
+  def descendingByRetweet: TweetList
+
   def incl(tweet: Tweet): TweetSet
 
-  /**
-   * Returns a new `TweetSet` which excludes `tweet`.
-   */
   def remove(tweet: Tweet): TweetSet
 
-  /**
-   * Tests if `tweet` exists in this `TweetSet`.
-   */
   def contains(tweet: Tweet): Boolean
 
-  /**
-   * This method takes a function and applies it to every element in the set.
-   */
   def foreach(f: Tweet => Unit): Unit
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-  /**
-   * The following methods are already implemented
-   */
+
+  def isEmpty: Boolean = true
+
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
 
   def contains(tweet: Tweet): Boolean = false
 
@@ -121,16 +67,21 @@ class Empty extends TweetSet {
   def remove(tweet: Tweet): TweetSet = this
 
   def foreach(f: Tweet => Unit): Unit = ()
+
+  def union(that: TweetSet): TweetSet = that
+
+  def mostRetweeted: Tweet = throw new java.util.NoSuchElementException("No tweets")
+
+  def
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
-  /**
-   * The following methods are already implemented
-   */
+  def isEmpty: Boolean = false
+
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet =
+    if (p(elem)) right.filterAcc(p, left.filterAcc(p, acc incl elem))
+    else right.filterAcc(p, left.filterAcc(p, acc incl elem))
 
   def contains(x: Tweet): Boolean =
     if (x.text < elem.text) left.contains(x)
