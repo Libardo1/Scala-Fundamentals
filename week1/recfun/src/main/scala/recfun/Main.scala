e237b894090767ac1c6a484a98fe5a6ece061f1c
Marvin Bertin <marvin.bertin@gmail.com>
2016-07-24 10:00:23 -0700

    update

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -11,20 +11,41 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
+    * Exercise 1
+    */
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 || c == r) 1
-    else pascal(c-1, r-1) + pascal(c, r-1)
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
-  
+
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def f(chars: List[Char], num: Int): Boolean = {
+      if (chars.isEmpty || num < 0)
+        num == 0
+      else if (chars.head == '(')
+        f(chars.tail, num + 1)
+      else if (chars.head == ')')
+        f(chars.tail, num - 1)
+      else
+        f(chars.tail, num)
+    }
+    f(chars, 0)
   }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def f(money: Int, coins: List[Int]): Int = {
+      if (coins.isEmpty) 0
+      else if (money - coins.head == 0) 1
+      else if (money - coins.head < 0) 0
+      else countChange(money - coins.head, coins) +
+        countChange(money, coins.tail)
+    }
+    f(money, coins.sorted)
+  }
+}
--- week1/recfun/src/main/scala/test.sc
+++ week1/recfun/src/main/scala/test.sc
@@ -1,5 +1,25 @@
 
+List(2,1,4,6).sorted
 
-"String".toList
+val coins = List(2, 1)
+
+def countChange(money: Int, coins: List[Int]): Int = {
+  def f(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty) 0
+    else if (money - coins.head == 0) 1
+    else if (money - coins.head < 0) 0
+    else countChange(money - coins.head, coins) +
+      countChange(money, coins.tail)
+  }
+  f(money, coins.sorted)
+}
+
+countChange(4, coins)
+
+
+
+
+
+
 
 
